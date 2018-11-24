0461cf1ae40944f4b1ba2205883f48e665c71d6a
vadim-dirsha <faqproshival@yandex.ru>
2018-11-24 16:57:39 +0300

    added entities

---
+++ src/main/java/ru/vadimdirsha/java/model/ICallCenter.java
@@ -0,0 +1,22 @@
+/**
+ * Vadim Dirsha
+ * Copyright (c) 2018 Java Open Source Project
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package ru.vadimdirsha.java.model;
+
+/**
+ * @author = Vadim Dirsha
+ * @date = 24.11.2018
+ */
+public interface ICallCenter {
+}
