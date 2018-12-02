3606a1cf5ac57552b03fbd60154cd30b373f0ad3
vadim-dirsha <faqproshival@yandex.ru>
2018-12-02 15:44:21 +0300

    add implementation to class "phoneThread"

--- src/main/java/ru/vadimdirsha/java/model/people/PhoneTread.java
+++ src/main/java/ru/vadimdirsha/java/model/people/PhoneTread.java
@@ -14,9 +14,38 @@
  */
 package ru.vadimdirsha.java.model.people;
 
+import org.apache.log4j.Logger;
+import ru.vadimdirsha.java.model.organization.Manager;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author = Vadim Dirsha
  * @date = 29.11.2018
  */
-public class PhoneTread {
+public class PhoneTread extends Thread {
+    private static Logger logger = Logger.getLogger(Manager.class);
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    private void hungUp() {
+        condition.signal();
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            lock.lock();
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage(), e);
+                interrupt();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
 }
