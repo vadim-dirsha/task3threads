edc3d9fc6d2887c5b2bc9d86e9472f42629c9703
vadim-dirsha <faqproshival@yandex.ru>
2018-12-03 15:39:58 +0300

    cleaning class "App"

--- src/main/java/ru/vadimdirsha/java/App.java
+++ src/main/java/ru/vadimdirsha/java/App.java
@@ -16,27 +16,5 @@ public class App {
         //ide doesn't copy log4j.properties to classes folder (classpath)
         //BasicConfigurator.configure();
 
-        ArrayList<Operator> operators = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            operators.add(new Operator(i, "Operator" + i));
-        }
-        ArrayList<Person> people = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            people.add(new Person("Man" + i, 1000 + i * 100, 200000, 10000, false));
-        }
-
-        Organization organization = Organization.getInstance();
-        organization.setUp();
-        LinkedList<Thread> threads = new LinkedList<>();
-        for (int i = 0; i < 1; i++) {
-            Thread thread = new PersonThread(new Person("CrazyMan" + i, 2000 + i * 10, 5000, false));
-            threads.add(thread);
-        }
-        people.forEach(e -> threads.add(new PersonThread(e)));
-        operators.forEach(e -> organization.getOperatorsRoom().addFreeOperator(e));
-        organization.startWork();
-        threads.forEach(Thread::start);
-        logger.info("main tread");
-
     }
 }
